97ae5c04bbf33949ef0211447fe674038b2d1e8b
stepans47 <stepan_sidorchuk@ukr.net>
2020-12-30 17:15:23 +0200

    Booking service and validator added

---
+++ PDR.PatientBooking.Service/BookingServices/Responses/GetPatientNextAppointmentResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PDR.PatientBooking.Service.BookingServices.Responses
+{
+    public class GetPatientNextAppointmentResponse
+    {
+        public Guid Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public long PatientId { get; set; }
+        public long DoctorId { get; set; }
+        public int SurgeryType { get; set; }
+    }
+}
